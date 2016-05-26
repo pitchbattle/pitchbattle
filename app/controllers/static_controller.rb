8828333962030f17a440309f7dc94d223c278ea9
Jerome Dalbert <jerome.dalbert@gmail.com>
2016-05-25 23:22:29 -0700

    Make the app work with Docker

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,5 +3,5 @@ class StaticController < ApplicationController
   def home
     render plain: 'Hello world!'
   end
-  
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # Do not use Rails 5's evented file watcher because it does not work with
+  # Docker on OS X (it will work with "Docker for Mac" when it gets released)
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 end
